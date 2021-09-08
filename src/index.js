e13e892878cb87f5c459489c8b797added159c71
Serji2158 <sergey.tyschenko@gmail.com>
2021-09-08 15:50:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,33 +6,38 @@ import countryCard from './templates/countryinfo.hbs';
 import countryList from './templates/countrylist.hbs';
 import debounce from 'lodash.debounce';
 import { alert } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
-import fetchCountry from './fetchCountries';
-  
-function onSearch (e){
-    e.preventDefault();
-  const input = e.target;
 
-   const valueSearch = input => {
+import fetchCountry from './fetchCountries';
+
+function onSearch(e) {
+  e.preventDefault();
+  const input = e.target;
+  refs.countryListRef.innerHTML = '';
+  if (!input) {
+    return;
+  }
+  const valueSearch = input => {
     fetchCountry(input)
-        .then(country => {
-           if(country.length === 1){
-             return refs.countryListRef.innerHTML = countryCard(country);
-           };
-           if(country.length >=2 && country.length <= 10){
-             return refs.countryListRef.innerHTML = countryList(country);
-           }
-            else{
-              refs.countryListRef.innerHTML = '';
-              return alert({
-              text:'Too many matches found. Please, enter a more specific query!',
-              delay: 500
-              });
-            };
-        })
-        .catch(error => {console.log(error)});
-   }
-    valueSearch(input.value);
+      .then(country => {
+        if (country.length === 1) {
+          return (refs.countryListRef.innerHTML = countryCard(country));
+        }
+        if (country.length >= 2 && country.length <= 10) {
+          return (refs.countryListRef.innerHTML = countryList(country));
+        } else {
+          return alert({
+            text: 'Too many matches found. Please, enter a more specific query!',
+            delay: 500,
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+  valueSearch(input.value);
 }
 
 refs.inputRef.addEventListener('input', debounce(onSearch, 500));
